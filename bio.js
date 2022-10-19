cdf2a4160c1d85abda1f13b5b5cfded74397eaab
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-19 20:40:26 +0800

    CRUD BioStat Program setup with map implementation

--- bio.js
+++ bio.js
@@ -2,7 +2,40 @@ const csvjson = require('csvjson')
 const fs = require('fs')
 const yargs = require('yargs/yargs')(process.argv.slice(2)).argv
 
-const data = fs.readFileSync('biostats.csv', 'utf8')
+class BioStat {
+  constructor(name, sex, age, height, weight) {
+    this.name = name[0].toUpperCase() + yargs.name.slice(1).toLowerCase()
+    this.sex = sex
+    this.age = Number(age)
+    this.height = Number(height)
+    this.weight = Number(weight)
+  }
+
+  isValidName() {
+    return typeof this.name === 'string' && this.name !== undefined
+  }
+
+  isValidSex() {
+    return typeof this.sex.toUpperCase() === 'string' && this.sex.toUpperCase()
+    && this.sex.length === 1
+    && 'MF'.includes(this.sex.toUpperCase())
+  }
+
+  isValidAge() {
+    return !Number.isNaN(this.age) && this.age >= 18
+  }
+
+  isValidHeight() {
+    return !Number.isNaN(this.height) && this.height > 0
+  }
+
+  isValidWeight() {
+    return typeof this.weight === 'number'
+  }
+}
+
+const csvFile = './biostats.csv'
+const data = fs.readFileSync(csvFile, 'utf8')
 const options = {
   delimiter: ',',
   quote: '"',
@@ -11,157 +44,132 @@ const options = {
 const [, flag] = Object.keys(yargs)
 const argLength = Object.keys(yargs).length
 const csvData = csvjson.toObject(data, options)
-yargs.name = yargs.name[0].toUpperCase() + yargs.name.slice(1).toLowerCase()
-class BioStat {
-  constructor(name, sex, age, height, weight) {
-    this.name = name
-    this.sex = sex
-    this.age = age
-    this.height = height
-    this.weight = weight
+const bioMap = new Map(csvData.map((object) => [object.name.toUpperCase(), object]))
+
+const isValidCreateUpdateParams = () => {
+  if (yargs.name === undefined || yargs.sex === undefined || yargs.age === undefined
+    || yargs.height === undefined || yargs.weight === undefined || argLength > 8) {
+    console.log('Invalid no. of arguments')
+    process.exit()
   }
 }
 
-const create = (bioArr, createBio) => {
-  if (createBio.name === undefined || createBio.sex === undefined || createBio.age === undefined
-    || createBio.height === undefined || createBio.weight === undefined || argLength > 8
-    || argLength < 4) {
+const isValidDeleteReadParams = () => {
+  if (yargs.name === undefined || argLength > 4) {
     console.log('Invalid no. of arguments')
+    process.exit()
+  }
+}
+
+const verify = (bioData) => {
+  if (bioData.isValidSex() === false) {
+    console.log('Invalid input for sex.')
     process.exit(1)
   }
-  for (let z = 0; z < bioArr.length; z += 1) {
-    if (bioArr[z].name.toUpperCase() === createBio.name.toUpperCase()) {
-      console.log('Bio data already exists')
-      process.exit(1)
-    }
-    if (createBio.sex.toUpperCase() !== 'F' && createBio.sex.toUpperCase() !== 'M') {
-      console.log('Invalid input for sex.')
-      process.exit(1)
-    }
-    if (parseInt(createBio.age, 10) < 18
-    || Number.isInteger(parseInt(createBio.age, 10)) === false
-    || Number.isNaN(parseInt(createBio.age, 10)) === true) {
-      console.log('Invalid age input!')
-      process.exit(1)
-    }
-    if (Number.isInteger(parseInt(createBio.height, 10)) === false
-    || Number.isNaN(parseInt(createBio.age, 10)) === true) {
-      console.log('Invalid height input.')
-      process.exit(1)
-    }
-    if (Number.isInteger(parseInt(createBio.weight, 10)) === false
-    || Number.isNaN(parseInt(createBio.age, 10)) === true) {
-      console.log('Invalid weight input.')
-      process.exit(1)
-    }
+  if (bioData.isValidAge() === false) {
+    console.log('Invalid age input!')
+    process.exit(1)
   }
-  console.log('BEFORE CREATION: \n', csvData)
-  bioArr.push(createBio)
-  fs.writeFileSync('biostats.csv', csvjson.toCSV(bioArr, { headers: 'key', delimiter: ',\t\t\t\t\t' }))
-  console.log('AFTER CREATION: \n', csvData)
+  if (bioData.isValidHeight() === false) {
+    console.log('Invalid height input.')
+    process.exit(1)
+  }
+  if (bioData.isValidWeight() === false) {
+    console.log('Invalid weight input.')
+    process.exit(1)
+  }
 }
 
-const read = (bioArr, bioName) => {
+const writeNewFile = (csvPath, newData) => {
+  try {
+    fs.writeFileSync(csvPath, csvjson.toCSV(Array.from(newData.values()), { headers: 'key', delimiter: ',\t\t\t\t\t' }))
+    return true
+  } catch (error) {
+    return false
+  }
+}
+const createBioStat = (map, createBio) => {
+  if (map.has(createBio.name.toUpperCase())) {
+    console.log('Bio data already exists')
+    process.exit(1)
+  }
+  verify(createBio)
+  map.set(createBio.name.toUpperCase(), createBio)
+  writeNewFile(csvFile, map)
+}
+
+const readBioStat = (map, bioName) => {
   if (bioName === undefined || argLength > 4) {
     console.log('Invalid no. of arguments')
     process.exit(1)
   }
-  let found = 0
-  for (let z = 0; z < bioArr.length; z += 1) {
-    if (bioArr[z].name.toUpperCase() === bioName.toUpperCase()) {
-      found = 1
-      console.log(bioArr[z])
-    }
-  }
-  if (found === 0) {
-    console.log('Bio data not found.')
-    process.exit(1)
+  if (map.has(bioName.toUpperCase())) {
+    const readData = map.get(bioName.toUpperCase())
+    console.log(`
+    ++++++++++++++ BIO DATA ++++++++++++++
+
+    · Name: ${readData.name}
+    · Sex: ${readData.sex === 'M' ? 'Male' : 'Female'}
+    · Age: ${readData.age} years old
+    · Height: ${readData.height} in ${readData.height * 2.54} cm
+    · Weight: ${readData.weight} lbs ${(readData.weight / 2.205).toFixed(2)} kilos
+
+    ++++++++++++++++++++++++++++++++++++++
+    `)
   }
 }
 
-const update = (bioArr, updateBio) => {
-  let found = 0
-  if (updateBio.name === undefined || updateBio.sex === undefined || updateBio.age === undefined
-    || updateBio.height === undefined || updateBio.weight === undefined || argLength > 8
-    || argLength < 4) {
-    console.log('Invalid no. of arguments')
+const updateBioStat = (map, updateBio) => {
+  if (!map.has(updateBio.name.toUpperCase())) {
+    console.log('Bio data not found')
     process.exit(1)
   }
-  for (let x = 0; x < bioArr.length; x += 1) {
-    if (bioArr[x].name.toUpperCase() === updateBio.name.toUpperCase()) {
-      found = 1
-      if (updateBio.sex.toUpperCase() !== 'F' && updateBio.sex.toUpperCase() !== 'M') {
-        console.log('Invalid input for sex.')
-        process.exit(1)
-      }
-      if (parseInt(updateBio.age, 10) < 18
-      || Number.isNaN(parseInt(updateBio.age, 10)) === true
-      || Number.isInteger(parseInt(updateBio.age, 10)) === false) {
-        console.log('Invalid age input!')
-        process.exit(1)
-      }
-      if (Number.isInteger(parseInt(updateBio.height, 10)) === false
-      || Number.isNaN(parseInt(updateBio.age, 10)) === true) {
-        console.log('Invalid height input.')
-        process.exit(1)
-      }
-      if (Number.isInteger(parseInt(updateBio.weight, 10)) === false
-      || Number.isNaN(parseInt(updateBio.age, 10)) === true) {
-        console.log('Invalid weight input.')
-        process.exit(1)
-      }
-      console.log('BEFORE UPDATE: \n', csvData)
-      const updateData = bioArr
-      updateData[x] = updateBio
-      fs.writeFileSync('biostats.csv', csvjson.toCSV(updateData, { headers: 'key', delimiter: ',\t\t\t\t\t' }))
-      console.log('AFTER UPDATE: \n', csvData)
-    }
-  }
-  if (found === 0) {
-    console.log('Bio data not found.')
-  }
+  verify(updateBio)
+  const existingData = map.set(updateBio.name.toUpperCase(), updateBio)
+  writeNewFile(csvFile, existingData)
 }
 
-const del = (bioArr, bioName) => {
-  if (bioName === undefined || argLength > 4) {
-    console.log('Invalid no. of arguments')
+const deleteBioStat = (map, bioName) => {
+  if (!map.has(bioName.toUpperCase())) {
+    console.log('Bio data not found')
     process.exit(1)
   }
-  let found = 0
-  for (let z = 0; z < bioArr.length; z += 1) {
-    if (bioArr[z].name.toUpperCase() === bioName.toUpperCase()) {
-      found = 1
-      console.log('BEFORE DELETION: \n', csvData)
-      bioArr.splice(z, 1)
-      fs.writeFileSync('biostats.csv', csvjson.toCSV(bioArr, { headers: 'key', delimiter: ',\t\t\t\t\t' }))
-      console.log('AFTER DELETION: \n', csvData)
-    }
-  }
-  if (found === 0) {
-    console.log('Bio data not found.')
-    process.exit(1)
-  }
+  map.delete(bioName.toUpperCase())
+  writeNewFile(csvFile, map)
 }
 
-switch (flag.toLowerCase()) {
+switch (flag) {
   case 'c':
-    create(
-      csvData,
+  {
+    isValidCreateUpdateParams()
+    createBioStat(
+      bioMap,
       new BioStat(yargs.name, yargs.sex.toUpperCase(), yargs.age, yargs.height, yargs.weight),
     )
     break
+  }
   case 'r':
-    read(csvData, yargs.name)
+  {
+    isValidDeleteReadParams()
+    readBioStat(bioMap, yargs.name)
     break
+  }
   case 'u':
-    update(
-      csvData,
+  {
+    isValidCreateUpdateParams()
+    updateBioStat(
+      bioMap,
       new BioStat(yargs.name, yargs.sex.toUpperCase(), yargs.age, yargs.height, yargs.weight),
     )
     break
+  }
   case 'd':
-    del(csvData, yargs.name)
+  {
+    isValidDeleteReadParams()
+    deleteBioStat(bioMap, yargs.name)
     break
+  }
   default:
-    console.log('Invalid argument. Please input -c, -r, -u, or -d.')
+    console.log('Invalid argument. Please input -c, -r, -u, or -d. The flag is case-sensitive.')
+    console.log(flag)
 }
